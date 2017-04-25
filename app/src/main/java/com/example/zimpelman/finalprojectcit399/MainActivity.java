fec013dd9a97237ba7ecd625971052c1227e03f3
czimpelman <crz3@pct.edu>
2017-04-25 15:16:40 -0400

    User Database

--- app/src/main/java/com/example/zimpelman/finalprojectcit399/MainActivity.java
+++ app/src/main/java/com/example/zimpelman/finalprojectcit399/MainActivity.java
@@ -1,37 +1,67 @@
 package com.example.zimpelman.finalprojectcit399;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvUsername, tvPassword;
+    Button btnCreate, btnLogin;
+    private List<User> userList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvUsername = (TextView)findViewById(R.id.tvUserName);
+        tvPassword = (TextView)findViewById(R.id.tvPassword);
 
+        btnCreate = (Button)findViewById(R.id.btnCreateAccount);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
 
+        final UserDB users = new UserDB(this);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String username = tvUsername.getText().toString();
+                String password = tvUsername.getText().toString();
+                User newUser = new User(username,password);
+                users.addUser(newUser);
+            }
+        });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                int tempId = -1;
+                String username="", password="";
+                int count = 0;
+                for(User user : userList){
+                    user = users.getUser(count);
+                    if(user.getUsername() == username){
+                        if(user.getPassword() == password){
+                            tempId = user.getUserId();
+                        }
+                    }
+                }
+                if(tempId != -1){
+                    Toast.makeText(getApplicationContext(), "Good Job", Toast.LENGTH_LONG);
+                }
+                //Intent i = new Intent(getApplicationContext(), UserAlarms.class);
+                //i.putExtra("User", tempId);
+                //startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/zimpelman/finalprojectcit399/UserDB.java
@@ -0,0 +1,142 @@
+package com.example.zimpelman.finalprojectcit399;
+
+/**
+ * Created by Christopher on 4/25/2017.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserDB extends SQLiteOpenHelper {
+
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "userDatabase";
+    private static final String USER_TABLE = "userTable";
+
+    //column names
+    private static final String KEY_ID = "id";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASSWORD = "password";
+
+    public UserDB(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE " + USER_TABLE + "("
+                + KEY_ID + " INTEGER PRIMARY KEY, "
+                + KEY_USERNAME + " TEXT,"
+                + KEY_PASSWORD + " TEXT;";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE);
+        onCreate(db);
+    }
+
+    public void deleteDB(Context context){
+        context.deleteDatabase(DB_NAME);
+    }
+
+    //CRUD
+
+    //Add new bird
+    void addUser(User user){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_USERNAME, user.getUsername());
+        values.put(KEY_PASSWORD, user.getPassword());
+
+        db.insert(USER_TABLE, null, values);
+        db.close();
+    }
+
+    // Getting single contact
+    User getUser(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(USER_TABLE, new String[] { KEY_ID,
+                        KEY_USERNAME, KEY_PASSWORD }, KEY_ID + "=?",
+                new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        User user = new User(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+        // return contact
+        return user;
+    }
+
+    public List<User> getAllUsers() {
+        List<User> userList = new ArrayList<User>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + USER_TABLE;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+
+                User user = new User();
+                user.setUserId(Integer.parseInt(cursor.getString(0)));
+                user.setUsername(cursor.getString(1));
+                user.setPassword(cursor.getString(2));
+                // Adding bird to list
+                userList.add(user);
+            } while (cursor.moveToNext());
+        }
+
+        // return bird list
+        return userList;
+    }
+
+    // Updating single contact
+    public int updateUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_USERNAME, user.getUsername());
+        values.put(KEY_PASSWORD, user.getPassword());
+
+        // updating row
+        return db.update(USER_TABLE, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(user.getUserId()) });
+    }
+
+    // Deleting single contact
+    public void deleteUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(USER_TABLE, KEY_ID + " = ?",
+                new String[] { String.valueOf(user.getUserId()) });
+        db.close();
+    }
+
+    // Getting contacts Count
+    public int getUserCount() {
+        String countQuery = "SELECT  * FROM " + USER_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        int count = cursor.getCount();
+        cursor.close();
+
+        // return count
+        return count;
+    }
+}
+
